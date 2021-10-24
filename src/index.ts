fa0e9867e253839846488e19a37af839c0a3e562
erbridge <f@erbridge.co.uk>
2021-10-24 17:08:15 +0200

    Require an object type for nullValuesAsUndefined

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 export type NullAsUndefined<T> = T extends null ? undefined : T;
 
-export type NullValuesAsUndefined<T> = {
+export type NullValuesAsUndefined<T extends Record<string, unknown>> = {
   [K in keyof T]: NullAsUndefined<T[K]>;
 };
 
 export const nullAsUndefined = <T>(value: T): NullAsUndefined<T> =>
   (value === null ? undefined : value) as NullAsUndefined<T>;
 
-export const nullValuesAsUndefined = <T>(obj: T): NullValuesAsUndefined<T> => {
-  return Object.entries(obj).reduce(
+export const nullValuesAsUndefined = <T extends Record<string, unknown>>(
+  obj: T
+): NullValuesAsUndefined<T> =>
+  Object.entries(obj).reduce(
     (result, [key, value]) => ({
       ...result,
       [key]: nullAsUndefined(value),
     }),
     {} as NullValuesAsUndefined<T>
   );
-};
